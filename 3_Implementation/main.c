ee761bf59598116cfe1a71e25f2e7c896a8988f8
99002757 <72466426+99002757@users.noreply.github.com>
2020-10-07 16:41:34 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,1 +1,17 @@
-
+ #include "header.h"
+int main (void)
+{
+        int s;
+    
+        Input:
+           printf("Input a number [ +(1), -(2), *(3), /(4), ^(5), !(6)] : ");
+           scanf("%d",&s);
+         
+           if (s > 7 | s < 1){
+                  printf("Please input again\n");
+                  goto Input;
+           }
+           sel_func (s);
+    
+           goto Input;
+}
